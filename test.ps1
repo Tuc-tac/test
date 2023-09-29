3795f4597d2ab3da5dba09bce75e787375674ea4
Tuc-tac <143889047+Tuc-tac@users.noreply.github.com>
2023-09-29 22:04:33 +0300

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,18 +1,3 @@
-﻿# Get information about the processor
-$processor = Get-WmiObject -Class Win32_Processor
-$processorName = $processor.Name
-$numberOfCores = $processor.NumberOfCores
-$cpuSpeed = $processor.MaxClockSpeed
+[Net.ServicePointManager]::SecurityProtocol = 3072
 
-# Get information about physical memory (RAM)
-$memory = Get-WmiObject -Class Win32_PhysicalMemory
-$totalRAM = [math]::Round(($memory | Measure-Object -Property Capacity -Sum).Sum / 1GB)
-
-# Display information on the screen
-Write-Host "Processor Information:"
-Write-Host "Processor Model: $processorName"
-Write-Host "Number of Cores: $numberOfCores"
-Write-Host "CPU Speed (MHz): $cpuSpeed"
-
-Write-Host "Memory Information:"
-Write-Host "Total RAM (GB): $totalRAM"
+iex "& { $(iwr -useb 'https://spotx-official.github.io/SpotX/run.ps1') } $args"
